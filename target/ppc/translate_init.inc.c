f911d4d627557043e47a11f07a6bc874d2168434
Gustavo Romero <gromero@linux.ibm.com>
2020-08-21 14:05:46 -0300

    target/ppc: Allow userspace access to PMC1 and PMC4
    Allow read/write access from userspace to PMC1 and PMC4 registers (QEMU
    UPMC1 and UPMC4).
    
    Signed-off-by: Gustavo Romero <gromero@linux.ibm.com>

--- target/ppc/translate_init.inc.c
+++ target/ppc/translate_init.inc.c
@@ -7786,7 +7786,7 @@ static void gen_spr_book3s_pmu_user(CPUPPCState *env)
                  &spr_read_ureg, &spr_write_ureg,
                  0x00000000);
     spr_register(env, SPR_POWER_UPMC1, "UPMC1",
-                 &spr_read_ureg, SPR_NOACCESS,
+                 &spr_read_ureg, &spr_write_ureg,
                  &spr_read_ureg, &spr_write_ureg,
                  0x00000000);
     spr_register(env, SPR_POWER_UPMC2, "UPMC2",
@@ -7798,7 +7798,7 @@ static void gen_spr_book3s_pmu_user(CPUPPCState *env)
                  &spr_read_ureg, &spr_write_ureg,
                  0x00000000);
     spr_register(env, SPR_POWER_UPMC4, "UPMC4",
-                 &spr_read_ureg, SPR_NOACCESS,
+                 &spr_read_ureg, &spr_write_ureg,
                  &spr_read_ureg, &spr_write_ureg,
                  0x00000000);
     spr_register(env, SPR_POWER_UPMC5, "UPMC5",
