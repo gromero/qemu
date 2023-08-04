0b28c770bf35eecef33a6a9c1425dab1d56e2f2b
Gustavo Romero <gustavo.romero@linaro.org>
2023-08-04 18:58:44 +0000

    Add ivshmem-flat device
    Add a new device, ivshmem-flat, which is similar to the ivshmem PCI but
    does not require a PCI bus. It's meant to be used on machines like those
    with Cortex-M MCUs, which usually lack a PCI/PCIe bus, e.g. lm3s6965evb
    and mps2-an385.
    
    The device currently only supports the sysbus bus.
    
    The following is an example on how to create the ivshmem-flat device on
    a Stellaris machine:
    
    $ qemu-system-arm -cpu cortex-m3 -machine lm3s6965evb -nographic
                      -net none -chardev stdio,id=con,mux=on
                      -serial chardev:con -mon chardev=con,mode=readline
                      -chardev socket,path=/tmp/ivshmem_socket,id=ivf
                      -device ivshmem-flat,x-irq-qompath=/machine/unattached/device[1]/nvic/unnamed-gpio-in[0],x-bus-qompath="/sysbus",chardev=ivf
                      -kernel zephyr_qemu.elf
    
    The new device, just like the ivshmem PCI device, supports both peer
    notification via hardware interrupts and a shared memory region.
    
    The IRQ QOM path for the target machine can be determined by creating
    the VM without the ivshmem-flat device, going to the QEMU console and
    listing the QOM nodes with 'info qom-tree'. In the Stellaris example
    above the input IRQ is in the NVIC IC.
    
    The MMRs for status and control (notification) are mapped to the MMIO
    region at 0x400FF000 (default), whilst the shared memory region start
    is mapped at addr. 0x40100000 (default), but both addresses can be set
    when creating the device by using 'x-bus-address-{mmr,shmem}' options,
    respectively.
    
    The device shared memory size can be set using the 'shmem-size' option
    and it defaults to 4 MiB, which is the default size of shmem allocated
    by the ivshmem server.
    
    Signed-off-by: Gustavo Romero <gustavo.romero@linaro.org>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -2938,6 +2938,7 @@ static void virt_machine_class_init(ObjectClass *oc, void *data)
     machine_class_allow_dynamic_sysbus_dev(mc, TYPE_VFIO_AMD_XGBE);
     machine_class_allow_dynamic_sysbus_dev(mc, TYPE_RAMFB_DEVICE);
     machine_class_allow_dynamic_sysbus_dev(mc, TYPE_VFIO_PLATFORM);
+    machine_class_allow_dynamic_sysbus_dev(mc, "ivshmem-flat");
 #ifdef CONFIG_TPM
     machine_class_allow_dynamic_sysbus_dev(mc, TYPE_TPM_TIS_SYSBUS);
 #endif
