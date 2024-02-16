33f73e6ff3c18de6b5e0a1a5ee6a4059874c833f
Gustavo Romero <gustavo.romero@linaro.org>
2024-02-16 15:44:53 +0100

    hw/arm: Allow some machines to use the ivshmem-flat device
    Allow Arm machine lm3s6965evb and the mps2 ones, like the mps2-an385, to
    use the ivshmem-flat device.
    
    Message-ID: <20231127052024.435743-2-gustavo.romero@linaro.org>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Signed-off-by: Gustavo Romero <gustavo.romero@linaro.org>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -84,6 +84,7 @@
 #include "hw/virtio/virtio-iommu.h"
 #include "hw/char/pl011.h"
 #include "qemu/guest-random.h"
+#include "hw/misc/ivshmem-flat.h"
 
 static GlobalProperty arm_virt_compat[] = {
     { TYPE_VIRTIO_IOMMU_PCI, "aw-bits", "48" },
@@ -2990,6 +2991,7 @@ static void virt_machine_class_init(ObjectClass *oc, void *data)
     machine_class_allow_dynamic_sysbus_dev(mc, TYPE_VFIO_AMD_XGBE);
     machine_class_allow_dynamic_sysbus_dev(mc, TYPE_RAMFB_DEVICE);
     machine_class_allow_dynamic_sysbus_dev(mc, TYPE_VFIO_PLATFORM);
+    machine_class_allow_dynamic_sysbus_dev(mc, TYPE_IVSHMEM_FLAT);
 #ifdef CONFIG_TPM
     machine_class_allow_dynamic_sysbus_dev(mc, TYPE_TPM_TIS_SYSBUS);
 #endif
