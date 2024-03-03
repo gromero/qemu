6f91ca4cfeeae728c6d899f0908ff74edae4a0a5
Gustavo Romero <gustavo.romero@linaro.org>
2024-03-03 15:33:07 +0000

    gdbstub: Add Xfer:siginfo:read stub
    Add stub to handle Xfer:siginfo:read packet query that requests the
    machine's siginfo data.
    
    This is used when GDB user executes 'print $_siginfo' and when the
    machine stops due to a signal, for instance, on SIGSEGV. The information
    in siginfo allows GDB to determiner further details on the signal, like
    the fault address/insn when the SIGSEGV is caught.
    
    Signed-off-by: Gustavo Romero <gustavo.romero@linaro.org>

--- gdbstub/gdbstub.c
+++ gdbstub/gdbstub.c
@@ -1632,6 +1632,8 @@ static void handle_query_supported(GArray *params, void *user_ctx)
     if (gdbserver_state.c_cpu->opaque) {
         g_string_append(gdbserver_state.str_buf, ";qXfer:auxv:read+");
     }
+
+    g_string_append(gdbserver_state.str_buf, ";qXfer:siginfo:read+");
 #endif
     g_string_append(gdbserver_state.str_buf, ";qXfer:exec-file:read+");
 #endif
@@ -1723,7 +1725,6 @@ static const GdbCmdParseEntry gdb_gen_query_set_common_table[] = {
     },
 };
 
-
 /* Arch-specific query table */
 static GdbCmdParseEntry *gdb_gen_query_table_arch = NULL;
 void set_gdb_gen_query_table_arch(GdbCmdParseEntry *table)
@@ -1788,6 +1789,12 @@ static const GdbCmdParseEntry gdb_gen_query_table[] = {
         .cmd_startswith = 1,
         .schema = "l,l0"
     },
+    {
+        .handler = gdb_handle_query_xfer_siginfo,
+        .cmd = "Xfer:siginfo:read::",
+        .cmd_startswith = 1,
+        .schema = "l,l0"
+     },
 #endif
     {
         .handler = gdb_handle_query_xfer_exec_file,
